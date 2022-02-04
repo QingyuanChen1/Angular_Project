ddae4eb6ab4ffcc01622de69c7f1a9427a1b559c
Qingyuan Chen <Qingyuan.Chen2@georgebrown.ca>
2022-02-04 12:59:13 -0500

    Final Push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav-component/nav.component';
+import { NavComponent } from './bottom-frame/nav.component';
 import { ProductsComponent } from './products/products.component';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './top-frame/footer.component';
 
 @NgModule({
   declarations: [
